9ee2e67d83b13791830f90aa15a2b824464dd5db
Achim @ Work <halfmann.achim@scheidt-bachmann.de>
2021-11-09 12:07:15 +0100

    Ping quick'n'dirty

---
+++ Syslog-Sender/MainForm.Designer.cs
@@ -0,0 +1,153 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Halfmann.Achim
+ * Date: 08.11.2021
+ * Time: 13:31
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Syslog_Sender
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Designer variable used to keep track of non-visual components.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button pingButton;
+        private System.Windows.Forms.Button sendButton;
+        private System.Windows.Forms.ComboBox sourceComboBox;
+        private System.Windows.Forms.TextBox targetTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel statusLabel;
+        
+        /// <summary>
+        /// Disposes resources used by the form.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing) {
+                if (components != null) {
+                    components.Dispose();
+                }
+            }
+            base.Dispose(disposing);
+        }
+        
+        /// <summary>
+        /// This method is required for Windows Forms designer support.
+        /// Do not change the method contents inside the source code editor. The Forms designer might
+        /// not be able to load this method if it was changed manually.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pingButton = new System.Windows.Forms.Button();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.sourceComboBox = new System.Windows.Forms.ComboBox();
+            this.targetTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.statusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pingButton
+            // 
+            this.pingButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pingButton.Location = new System.Drawing.Point(192, 88);
+            this.pingButton.Name = "pingButton";
+            this.pingButton.Size = new System.Drawing.Size(75, 23);
+            this.pingButton.TabIndex = 0;
+            this.pingButton.Text = "Ping";
+            this.pingButton.UseVisualStyleBackColor = true;
+            this.pingButton.Click += new System.EventHandler(this.PingButtonClick);
+            // 
+            // sendButton
+            // 
+            this.sendButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.sendButton.Location = new System.Drawing.Point(192, 136);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(75, 23);
+            this.sendButton.TabIndex = 1;
+            this.sendButton.Text = "Send";
+            this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.SendButtonClick);
+            // 
+            // sourceComboBox
+            // 
+            this.sourceComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.sourceComboBox.FormattingEnabled = true;
+            this.sourceComboBox.Location = new System.Drawing.Point(24, 40);
+            this.sourceComboBox.Name = "sourceComboBox";
+            this.sourceComboBox.Size = new System.Drawing.Size(152, 21);
+            this.sourceComboBox.TabIndex = 2;
+            // 
+            // targetTextBox
+            // 
+            this.targetTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.targetTextBox.Location = new System.Drawing.Point(24, 88);
+            this.targetTextBox.Name = "targetTextBox";
+            this.targetTextBox.Size = new System.Drawing.Size(152, 20);
+            this.targetTextBox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(24, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Source";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(24, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Target/Destination";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 239);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(282, 22);
+            this.statusStrip1.TabIndex = 6;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(42, 17);
+            this.statusLabel.Text = "Status!";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 261);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.targetTextBox);
+            this.Controls.Add(this.sourceComboBox);
+            this.Controls.Add(this.sendButton);
+            this.Controls.Add(this.pingButton);
+            this.Name = "MainForm";
+            this.Text = "Syslog-Sender";
+            this.Load += new System.EventHandler(this.MainFormLoad);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
---
+++ Syslog-Sender/MainForm.cs
@@ -0,0 +1,112 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Halfmann.Achim
+ * Date: 08.11.2021
+ * Time: 13:31
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+
+namespace Syslog_Sender
+{
+    /// <summary>
+    /// Description of MainForm.
+    /// </summary>
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            //
+            // The InitializeComponent() call is required for Windows Forms designer support.
+            //
+            InitializeComponent();
+            
+            //
+            // TODO: Add constructor code after the InitializeComponent() call.
+            //
+        }
+        
+        void MainFormLoad(object sender, EventArgs e)
+        {
+            statusLabel.Text = "";
+            
+            //TODO: Fix Quick'n'dirty-Code
+            
+            // Lokale IP-Adressen ermitteln
+            IPAddress[] addressList = Dns.GetHostEntry(Dns.GetHostName()).AddressList;
+            foreach (IPAddress ipAddress in addressList)
+            {
+                // Nur IPv4
+                if (ipAddress.AddressFamily == AddressFamily.InterNetwork)
+                    sourceComboBox.Items.Add(ipAddress.ToString());
+            }
+            //foreach (IPAddress ipAddress in addressList)
+            //{
+            //    // Nur IPv6
+            //    if (ipAddress.AddressFamily == AddressFamily.InterNetworkV6)
+            //        sourceComboBox.Items.Add(ipAddress.ToString());
+            //}
+            
+            //TODO: und letzten aktiven Eintrag wieder aktivieren
+            
+            //TODO: letzten Eintrag in Target wieder eintragen
+        }
+        
+        void PingButtonClick(object sender, EventArgs e)
+        {
+            statusLabel.Text = "";
+
+            if (targetTextBox.TextLength == 0)
+                return;
+            string address = targetTextBox.Text;
+            const int timeout = 2000;
+
+            //TODO: Worker-Thread
+            
+            try
+            {
+                Ping ping = new Ping();
+                PingReply pingReply = ping.Send(address, timeout);
+
+                switch (pingReply.Status)
+                {
+                    case IPStatus.Success:
+                        statusLabel.Text = "Ok, " + address + " ist erreichbar.";
+                        break;
+                        
+                    case IPStatus.TimedOut:
+                    case IPStatus.TimeExceeded:
+                        statusLabel.Text = "Zeitüberschreitung!";
+                        break;
+                    case IPStatus.DestinationHostUnreachable:
+                        statusLabel.Text = "Unerreichbar!";
+                        break;
+                    case IPStatus.HardwareError:
+                        statusLabel.Text = "Hardware-Fehler!";
+                        break;
+                    default:
+                        statusLabel.Text = "Nix Ping!";
+                        break;
+                }
+            }
+            catch (PingException)
+            {
+               statusLabel.Text = "Ungültige Adresse!";
+            }
+            catch (ArgumentException)
+            {
+               statusLabel.Text = "Ungültige Adresse!";                
+            }
+        }
+        
+        void SendButtonClick(object sender, EventArgs e)
+        {
+            statusLabel.Text = "";          
+        }
+    }
+}
